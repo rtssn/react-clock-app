09055e8bb038a947758347f68f1249494a5794e8
motoki <krkrmfmf@outlook.jp>
2022-05-18 23:03:30 +0900

    時計表示を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useEffect, useState} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => {
+    const DayList = ['SUN', 'MON', 'THU', 'WED', 'TUE', 'FRI', 'SAT'];
+
+    const [year, setYear] = useState('');
+    const [month, setMonth] = useState('');
+    const [date, setDate] = useState('');
+    const [day, setDay] = useState('');
+    const [hour, setHour] = useState('');
+    const [minute, setMinute] = useState('');
+    const [second, setSecond] = useState('');
+
+    useEffect(() => {
+        const timerId = setInterval(() => {
+            tick();
+        }, 1000);
+
+        return () => {
+            clearInterval(timerId);
+        };
+    });
+
+    const tick = () => {
+        const currentDate = new Date();
+
+        const year = currentDate.getFullYear().toString();
+        const month = ('00' + (currentDate.getMonth() + 1)).slice(-2);
+        const date = ('00' + currentDate.getDate()).slice(-2);
+        setYear(year);
+        setMonth(month);
+        setDate(date);
+
+        const day = DayList[currentDate.getDay()];
+        setDay(day);
+
+        const hour = ('00' + currentDate.getHours()).slice(-2);
+        const minute = ('00' + currentDate.getMinutes()).slice(-2);
+        const second = ('00' + currentDate.getSeconds()).slice(-2);
+
+        setHour(hour);
+        setMinute(minute);
+        setSecond(second);
+    };
+
+    return (
+        <div className='App'>
+            <div className='clock'>
+                <div className='date'>
+                    {year} / {month} / {date} ({day})
+                </div>
+                <div className='time'>
+                    {hour} : {minute}
+                    <span className='second'>{second}</span>
+                </div>
+            </div>
+        </div>
+    );
+};
 
 export default App;
