088b2092f3aa7a40abdaf0288d45ad7a2956b711
motoki <krkrmfmf@outlook.jp>
2022-05-19 00:07:52 +0900

    天気予報取得追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import {useEffect, useState} from 'react';
+import Axios from 'axios';
+import Config from './config';
 import './App.css';
 
 const App: React.FC = () => {
-    const DayList = ['SUN', 'MON', 'THU', 'WED', 'TUE', 'FRI', 'SAT'];
-
     const [year, setYear] = useState('');
     const [month, setMonth] = useState('');
     const [date, setDate] = useState('');
@@ -13,6 +13,49 @@ const App: React.FC = () => {
     const [second, setSecond] = useState('');
 
     useEffect(() => {
+        const DayList = ['SUN', 'MON', 'THU', 'WED', 'TUE', 'FRI', 'SAT'];
+        const tick = () => {
+            const currentDate = new Date();
+
+            const year = currentDate.getFullYear().toString();
+            const month = ('00' + (currentDate.getMonth() + 1)).slice(-2);
+            const date = ('00' + currentDate.getDate()).slice(-2);
+            setYear(year);
+            setMonth(month);
+            setDate(date);
+
+            const day = DayList[currentDate.getDay()];
+            setDay(day);
+
+            const hour = ('00' + currentDate.getHours()).slice(-2);
+            const minute = ('00' + currentDate.getMinutes()).slice(-2);
+            const second = ('00' + currentDate.getSeconds()).slice(-2);
+
+            setHour(hour);
+            setMinute(minute);
+            setSecond(second);
+        };
+
+        const getGeolocation = () => {
+            navigator.geolocation.getCurrentPosition((possition) => {
+                console.log(possition);
+
+                const lat = possition.coords.latitude;
+                const lon = possition.coords.longitude;
+
+                Axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${Config.openWeatherMapApiKey}`)
+                    .then((response) => {
+                        console.log(response);
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            });
+        };
+
+        tick();
+        getGeolocation();
+
         const timerId = setInterval(() => {
             tick();
         }, 1000);
@@ -25,35 +68,7 @@ const App: React.FC = () => {
             clearInterval(timerId);
             clearInterval(geolocationTimerId);
         };
-    });
-
-    const tick = () => {
-        const currentDate = new Date();
-
-        const year = currentDate.getFullYear().toString();
-        const month = ('00' + (currentDate.getMonth() + 1)).slice(-2);
-        const date = ('00' + currentDate.getDate()).slice(-2);
-        setYear(year);
-        setMonth(month);
-        setDate(date);
-
-        const day = DayList[currentDate.getDay()];
-        setDay(day);
-
-        const hour = ('00' + currentDate.getHours()).slice(-2);
-        const minute = ('00' + currentDate.getMinutes()).slice(-2);
-        const second = ('00' + currentDate.getSeconds()).slice(-2);
-
-        setHour(hour);
-        setMinute(minute);
-        setSecond(second);
-    };
-
-    const getGeolocation = () => {
-        navigator.geolocation.getCurrentPosition((possition) => {
-            console.log(possition);
-        });
-    };
+    }, []);
 
     return (
         <div className='App'>
